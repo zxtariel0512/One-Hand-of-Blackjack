ebaaa836341360bd8c6e19774313593280b7fc37
Xintong Zhu <arielzhu@xintongs-mbp.myfiosgateway.com>
2021-04-12 14:26:06 -0400

    after clicking hit

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -6,6 +6,7 @@ function main(){
     let computer = [];
     let userScore = 0;
     let computerScore = 0;
+    let winner = -1;
     
     // make the form disappear after clicking the play button
     const playButton = document.querySelector('.playBtn');
@@ -20,9 +21,13 @@ function main(){
         const input = document.querySelector('#startValues').value.split(',');
         const shuffled = generateCards(input);
         computer.push(shuffled[0]);
-        user.push(shuffled[1]);
-        computer.push(shuffled[2]);
-        user.push(shuffled[3]);
+        shuffled.splice(0, 1);
+        user.push(shuffled[0]);
+        shuffled.splice(0, 1);
+        computer.push(shuffled[0]);
+        shuffled.splice(0, 1);
+        user.push(shuffled[0]);
+        shuffled.splice(0, 1);
 
         // create buttons
         const hit = document.createElement('button');
@@ -31,8 +36,26 @@ function main(){
         const stand = document.createElement('button');
         stand.classList.add('stand');
         stand.appendChild(document.createTextNode('stand'));
-        game.appendChild(hit);
-        game.appendChild(stand);
+
+        function gameEnd(winner, computerDiv, computerScore){
+            hit.classList.add('disappear');
+            stand.classList.add('disappear');
+            const result = document.createElement('h4');
+            result.classList.add('result');
+            if(winner === -1){
+                result.appendChild(document.createTextNode('Oops, tie! 😕'));
+            } else if(winner === 0){
+                result.appendChild(document.createTextNode('User lost, saaaad! 😭'));
+            } else{
+                result.appendChild(document.createTextNode('Bravo! User wins! 🤠'));
+            }
+            game.appendChild(result);
+            const computerHand = computerDiv.querySelector('.card');
+            console.log(computerHand);
+            // computerDiv.removeChild(document.getElementById('unknownCard'))
+            computerDiv.replaceChild(createCard(computer[computer.length - 1].face, computer[computer.length - 1].suit), document.getElementById('unknownCard'));
+            computerDiv.firstChild.firstChild.nodeValue = `Computer Hand -- Total: ${computerScore}`;
+        }
         
 
         // scores
@@ -44,20 +67,48 @@ function main(){
         userH.appendChild(document.createTextNode(`Player Hand -- Total: ${userScore}`));
         const computerH = document.createElement('h4');
         computerH.appendChild(document.createTextNode(`Computer Hand -- Total: ?`));
-        game.appendChild(computerH);
         
         const computerDiv = document.createElement('div');
         computerDiv.classList.add('computer');
         game.appendChild(computerDiv);
-        computerDiv.appendChild(createCard(computer[0].face, computer[0].suit, false));
+        computerDiv.appendChild(computerH);
+        const unknownCard = createCard(computer[0].face, computer[0].suit, false);
+        unknownCard.setAttribute('id', 'unknownCard');
+        computerDiv.appendChild(unknownCard);
         computerDiv.appendChild(createCard(computer[1].face, computer[1].suit));
         userH.classList.add('userScore');
         game.appendChild(userH);
         const userDiv = document.createElement('div');
         userDiv.classList.add('user');
         game.appendChild(userDiv);
+        userDiv.appendChild(userH);
         userDiv.appendChild(createCard(user[0].face, user[0].suit));
         userDiv.appendChild(createCard(user[1].face, user[1].suit));
+
+        game.appendChild(hit);
+        game.appendChild(stand);
+
+        // create events for hit and stand
+        hit.addEventListener('click', clickHit);
+        stand.addEventListener('click', clickStand);
+        function clickHit(evt){
+            user.push(shuffled[0]);
+            userDiv.appendChild(createCard(user[user.length - 1].face, user[user.length - 1].suit))
+            shuffled.splice(0, 1);
+            userScore = updateScore(user);
+            userDiv.firstChild.firstChild.nodeValue = `Player Hand -- Total: ${userScore}`;
+            console.log(userScore);
+            if(userScore > 20){
+                winner = 0;
+                gameEnd(winner, computerDiv, computerScore);
+            }
+        }
+        function clickStand(evt){
+            while(computerScore < 15){
+                computer.push(shuffled[0]);
+                computerDiv
+            }
+        }
     }
 }
 
